9dd3fd14c9fd652bb43bdb36fbd84ce2e4650d24
Jonathan <johnjonathan948@gmail.com>
2021-05-24 00:40:34 -1200

    first commit

---
+++ contact.js
@@ -0,0 +1,28 @@
+
+
+const wrapper = document.querySelector('.wrapper'),
+form = wrapper.querySelectorAll('.form'),
+submitInput = form[0].querySelector('input[type="submit"]');
+
+function getDataForm(e) {
+    e.preventDefault();
+
+    let formData = new FormData(form[0]);
+
+    alert(formData.get('nameFied') + ' Your massage is sent as ' + formData.get('emailFied') + ' - ' + formData.get('massageField'));
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    submitInput.addEventListener('click', getDataForm, false);
+}, false);
+
+
+
+
+
+
+
+
+
